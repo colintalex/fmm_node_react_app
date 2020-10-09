d5c58720aad8312c60c0fdbfa41c737f6fb1c9a1
Colin Alexander <colintalex@gmail.com>
2020-10-09 05:20:21 -0600

    Converts pre-built App class component to new functional component with hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [apiResponse, setApiResponse] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/testAPI")
+      .then(resp => {
+        setApiResponse(resp.data);
+        console.log(resp)
+        console.log(apiResponse);
+      })
+      .catch(err => console.log(err))
+  }, [apiResponse.length]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{apiResponse}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
