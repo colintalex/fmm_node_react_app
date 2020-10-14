d691a159df533ca63fd3670f5223993dda731e18
Colin Alexander <colintalex@gmail.com>
2020-10-14 06:05:32 -0600

    Adds Google Maps with Hooks to USDA(FMM) api for markets close by

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Switch, Route } from "react-router-dom";
-import logo from './logo.svg';
-import axios from 'axios'
 import './App.css';
-import MainMap from './components/MainMap/MainMap';
+import GoogleApiWrapper from './components/MainMap/MainMap';
 import Home from './components/Home/Home';
 import SignIn from './components/SignIn/SignIn'
 
 const App = () => {
-  const [apiResponse, setApiResponse] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/testAPI")
-      .then(resp => {
-        setApiResponse(resp.data);
-        console.log(resp)
-        console.log(apiResponse);
-      })
-      .catch(err => console.log(err))
-  }, [apiResponse.length]);
-  
   return (
-    <div>
-      <Switch>
-        <Route path="/main_map">
-          <MainMap />
-        </Route>
-        <Route path='/login'>
-          <SignIn />
-        </Route>
-        <Route exact path="/"> {/*Adding the exact path nullifies the default value route, but prevents displaying components that match route*/}
-          <Home />
-        </Route>
-      </Switch>
-    </div>
+        <Switch>
+          <Route path="/main_map">
+            <GoogleApiWrapper />
+          </Route>
+          <Route path='/login'>
+            <SignIn />
+          </Route>
+          <Route exact path="/"> {/*Adding the exact path nullifies the default value route, but prevents displaying components that match route*/}
+            <Home />
+          </Route>
+        </Switch>
   );
 }
 
--- client/src/components/MainMap/MainMap.js
+++ client/src/components/MainMap/MainMap.js
@@ -1,13 +1,125 @@
-import React from 'react';
-import { BrowserRouter, Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import GoogleMap from 'google-map-react'
+import Sidebar from 'react-sidebar';
+import axios from 'axios'
+import styled from 'styled-components'
+import shouldPureComponentUpdate from 'react-pure-render/function';
+import { Redirect } from 'react-router-dom';
+
+const MapWrapper = styled.div`
+    height: 80vh;
+    width: 100%;
+`
+
+const Marker = styled.div`
+    position: 'absolute',
+    width: 20px;
+    height: 20px;
+    top: 20px;
+    left: 20px;
+
+    border: '5px solid #f44336';
+    borderRadius: 1px;
+    backgroundColor: 'white';
+    textAlign: 'center';
+    color: '#3f51b5';
+    fontSize: 16;
+    fontWeight: 'bold';
+    padding: 4;
+`
+const MyGreatPlace = (props) => {
+    return (
+        <Marker>
+            
+        </Marker>
+    );
+}
+
 
 const MainMap = () => {
+    const [center, setCenter] = useState({lat: 39.741667, lng: -104.978649});
+    const [lat, setLat] = useState(39.741667);
+    const [lng, setLng] = useState(-104.978649);
+    const [markets, setMarkets] = useState([]);
+    const [zoom, setZoom] = useState(11);
+    const [marks, setMarks] = useState([]);
+
+    useEffect(() => {
+        axios.post("http://localhost:5000/", {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+        },
+        query: `query($lat: Float!, $lng: Float!, $radius: Int!, $products: [String!] $date: String!){ marketsByCoords(lat: $lat, lng: $lng, radius: $radius, products: $products, date: $date ) { 
+                location 
+                markets {
+                fmid 
+                marketname
+                latitude
+                longitude
+                website
+                distance
+                seasonDates
+                street
+                city
+                state
+                zip
+                products {
+                    name
+                }
+                }
+            }
+        }`,
+        variables: {
+            lat: lat,
+            lng: lng,
+            radius: 50,
+            products: [],
+            date: ""
+        }
+        })
+        .then(resp => {
+            setMarkets(resp.data.data.marketsByCoords.markets);
+            // setLocation(resp.data.data.marketsByCoords.location);
+        })
+        .catch(err => console.log(err))
+    }, [lat, lng]);
+
+    useEffect(() => {
+        setMarks(markets.map((market) => {
+            const { latitude, longitude, fmid } = market;
+
+            return (
+                <div key={fmid} lat={latitude} lng={longitude} style={{color: 'red', height: '20px', backgroundColor: 'red', width: '20px',}}/>
+            )
+        }))
+    }, [markets]);
+
+    const _onChange = ((data) => {
+        setCenter(data.center);
+        setLat(data.center.lat);
+        setLng(data.center.lng);
+    });
+
     return (
         <div>
-            <h1>Main Map Page</h1>
-            <Link to="/">Home</Link>
-        </div>
+            <Sidebar
+                sidebar={<b>Login to view favorites</b>}
+                docked='true'
+                styles={{ sidebar: { background: "white" } }}
+            />
+            <MapWrapper>
+                <GoogleMap
+                bootstrapURLKeys={{ key: 'AIzaSyC9D6rE1m0f2aAKVCYWfWoIuHNNRcr-dvE'}}
+                center={center}
+                defaultZoom={zoom}
+                onChange={_onChange}
+                >
+                    {marks}
+                </GoogleMap>
+            </MapWrapper>
+        </div>    
     );
-};
+}
 
-export default MainMap
+export default MainMap;
