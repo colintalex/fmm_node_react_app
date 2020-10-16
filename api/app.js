6c6455c098d9a97a5bbb0439e37e42468471c912
Colin Alexander <colintalex@gmail.com>
2020-10-16 08:23:08 -0600

    Adds auth route to app

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 var testAPIRouter = require("./routes/testAPI");
 var bodyParser = require('body-parser');
 var app = express();
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
 
---
+++ api/routes/auth.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const { response } = require('../app');
+var router = express.Router();
+var User = require('../models/User');
+var bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/', (req, res) => {
+  // Validation goes here, correct info
+    console.log(req.body);
+    if (!req.body.password || !req.body.email) return res.status(409).send({error: 'All fields must be completed'});
+    // Check for existing user
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if(!user) return res.status(400).send({error: 'User does not exist'});
+
+        //Validate password
+        bcrypt.compare(req.body.password, user.password)
+        .then(isMatch => {
+            if(!isMatch) return res.status(400).send({error: 'Invalid Credentials'})
+
+            jwt.sign(
+                {id: user.id},
+                process.env.JWT_SECRET,
+                { expiresIn: 3600 }, (err, token) => {
+                if(err) throw err;
+                res.json({
+                    user: {
+                    user_name: user.user_name,
+                    email: user.email,
+                    favorites: user.favorites
+                    }, token: token
+                })
+
+                }
+            )
+        })
+
+    })
+    .catch((err) => res.status(409).send({error: err}));
+});
+
+module.exports = router;
