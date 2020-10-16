ab695b93154c8b1220735c1a0e1bd33c069a2b26
Colin Alexander <colintalex@gmail.com>
2020-10-16 12:50:35 -0600

    CSS adjustments to Detail Pane

--- client/src/components/MainMap/SearchWrapper.js
+++ client/src/components/MainMap/SearchWrapper.js
@@ -4,35 +4,46 @@ import { useForm } from 'react-hook-form';
 import Geocode from 'react-geocode'
 
 
-Geocode.setApiKey('AIzaSyC9D6rE1m0f2aAKVCYWfWoIuHNNRcr-dvE')
+Geocode.setApiKey(process.env.GOOGLE_API_KEY)
 
 const SearchBar = styled.div`
     position: fixed;
     display: block;
-    width: 40%;
+    width: 30%;
     height: 30px;
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
     z-index: 2;
-    margin-top: 15px;
-    margin-left: 20px;
+    margin: 15px auto;
     align-content: center;
 `
 
 const StyledInput = styled.input`
     width: 80%;
     font-size: 20px;
-    opacity: 80%;
+    opacity: 95%;
     height: 25px;
+    background-color: #38A3A5 !important;
+    border-radius: 20px;
+    color: #fff;
+    outline: none !important;
+    padding-left: 15px;
+
 `
 
 const StyledSubmit = styled.button`
-    display: block;
-    width: 15%;
-    height: 20px;
-    opacity: 85%;
+    display: inline-block;
+    position: fixed;
+    width: 75px;
+    height: 30px;
+    opacity: 95%;
+    font-size: 18px;
+    background-color: #ef8275;
+    border: 3px solid #38A3A5;
+    border-radius: 20px;
+    margin-left: 5px;
 `
 
 const SearchWrapper = (({onSearchChange}) => {
@@ -53,7 +64,7 @@ const SearchWrapper = (({onSearchChange}) => {
             <form onSubmit={handleSubmit(_onSubmit)}>
                 <StyledInput type='text' 
                     onChange={(event) => onSearchChange(event.target.value)}
-                    placeholder='Search by City or State' 
+                    placeholder='Search by City, State' 
                     ref={register}
                     name='location'   
                 ></StyledInput>
