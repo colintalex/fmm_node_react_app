2e42aa0f99142dbc684e4ba810515ed6924ea475
Colin Alexander <colintalex@gmail.com>
2020-10-16 17:01:50 -0600

    Unused, delete soon

---
+++ client/src/components/MainMap/Details/FavoriteComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FavoriteComponent = ((props) => {
+    console.log(props)
+    return (
+        <button></button>
+    )
+});
+
+export default FavoriteComponent;
