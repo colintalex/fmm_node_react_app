0cad0bce3fa3845333260f96018a1acec1882008
Colin Alexander <colintalex@gmail.com>
2020-10-19 21:32:29 -0600

    Rebuild prod build to reflect changes part 2

---
+++ api/build/precache-manifest.be0638fe7a86d770ed61a58c5d6bbc69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f957095aebe3ddf753ad62391377db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96aa3d3f8a40cc91580f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc271c2c1fda02b4426e",
+    "url": "/static/js/2.6523b643.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.6523b643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96aa3d3f8a40cc91580f",
+    "url": "/static/js/main.4d6536a5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "03cb5e4172574620c356efec16e8508f",
+    "url": "/static/media/background-home.03cb5e41.png"
+  },
+  {
+    "revision": "167d65a5ae00cfba429bfdc33177c815",
+    "url": "/static/media/fmm_banner.167d65a5.png"
+  }
+]);
