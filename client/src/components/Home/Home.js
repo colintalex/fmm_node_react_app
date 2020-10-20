0f4114e4601582191607bed70e2d4fe2ae491aeb
Colin Alexander <colintalex@gmail.com>
2020-10-19 20:39:48 -0600

    Register and Home page mostly structured

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,35 +3,47 @@ import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 const backgroundImage = require('../../background-home.png')
+const Banner = require('./fmm_banner.png')
 
 const HomeWrapperStyled = styled.div`
-    margin-top: -22px;
     height: 100vh;
     width: 100%;
     text-align: center;
     backdrop-filter: blur(3px)  brightness(95%);
 `
 const HeaderWrapperStyled = styled.div`
-    height: 10vh;
+    height: 100px;
     width: 100%;
     background: rgb(203,104,93);
     background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
+    margin-bottom: 60px;
 `
 
 const InteractionWrapperStyled = styled.div`
-    height: 45vh;
-    width: 25vw;
-    margin: 25vh auto;
-    padding: 15px 0;
-    background: linear-gradient(180deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
+    height: auto;
+    width: auto;
+    margin: 3px auto;
+    background: linear-gradient(0deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
     font-family: arial;
     font-size: 20px;
-    padding: 0 10px;
     border: 1px solid #888;
-    border-radius: 20px;
+    border-radius: 60px;
 `
+
+const PrimaryInteractionWrapperStyled = styled.div`
+    height: auto;
+    width: auto;
+    margin: 0 auto 10px auto;
+    padding: 5px 10px;
+    background: linear-gradient(0deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
+    font-family: arial;
+    font-size: 20px;
+    border: 1px solid #888;
+    border-radius: 60px;
+`
+
 const FormStyled = styled.form`
-    padding: 10px 0;
+    padding: 5px 0;
 `
 const InputStyled = styled.input`
     font-size: 1.2em;
@@ -39,9 +51,11 @@ const InputStyled = styled.input`
     padding: 0 10px;
     border: 3px solid #38A3A5;
     border-radius: 20px;
+    background: rgb(203,104,93);
+    background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
 `
 const LabelStyled = styled.label`
-
+    
 `
 
 const InputButtonStyled = styled.input`
@@ -56,21 +70,59 @@ const InputButtonStyled = styled.input`
 `
 
 const ButtonStyled = styled.button`
-    display: inline-block;
+    display: block;
+    margin: 10px auto;
+    padding: 11px 20px;
     font-size: 1em;
-    width: 75%;
+    width: auto;
     height: 55px;
     border: 3px solid rgb(203,104,93);
     border-radius: 20px;
     background: rgb(203,104,93);
-    background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
-
+    background: linear-gradient(180deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
 `
 const Background = styled.div`
     background-image: url('${backgroundImage}');
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
+    height: 100vh;
+`
+
+const StyledHeading = styled.h2`
+    margin: 10px 5px;
+`
+
+const ButtonWrapperStyle = styled.div`
+    width: 15vw;
+    margin: 15px auto;
+    padding: 25px 0px;
+    background: rgb(203,104,93);
+    background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
+    font-family: arial;
+    font-size: 20px;
+    padding: 0 10px;
+    border: 1px solid #888;
+    border-radius: 40px;
+`
+
+const BannerStyled = styled.img`
+    width: 30vw;
+    margin-top: 11px;
+`
+
+const InteractionBackground = styled.div`
+    height: auto;
+    width: 30vw;
+    margin: 100px auto;
+    padding: 15px;
+    font-family: arial;
+    font-size: 20px;
+    border: 1px solid #888;
+    border-radius: 60px;
+    margin: 0 auto;
+    background: rgb(203,104,93);
+    background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
 `
 
 const Home = ({ handleAuthentication, currentUser }) => {
@@ -90,21 +142,31 @@ const Home = ({ handleAuthentication, currentUser }) => {
         <Background>
             <HomeWrapperStyled>
                 <HeaderWrapperStyled className='header-wrapper'>
-                    <h1>Welcome to Find My Market!</h1>
+                        <BannerStyled src={Banner}/>
                 </HeaderWrapperStyled>
-                <InteractionWrapperStyled>
-                    <FormStyled onSubmit={handleSubmit(_handleAuthenticationRedirect)}>
-                        <LabelStyled>I heard you were looking for farmers markets?<br/> We got you covered! Sign in, sign up or go as a ghost!</LabelStyled>
-                        <hr/>
-                        <LabelStyled>Sign In To View Favorite Markets</LabelStyled>
-                        <InputStyled type='text' name='email' ref={register} placeholder='Email'></InputStyled><br/>
-                        <InputStyled type='password' name='password' ref={register} placeholder='Password'></InputStyled><br/>
-                        <InputButtonStyled type='submit'></InputButtonStyled>
-                    </FormStyled>
-                    <hr/>
+                    <InteractionBackground>
+                        <PrimaryInteractionWrapperStyled>
+                                <LabelStyled>
+                                    <StyledHeading>Looking for farmers markets?</StyledHeading>
+                                    <StyledHeading>We got you covered!</StyledHeading>
+                                    <p>Using an enhanced API, FindMyMarket can provide what you're looking for in a market.. faster and easier!<br/>
+                                    Sign in, sign up or go as a guest!</p>
+                                </LabelStyled>
+                        </PrimaryInteractionWrapperStyled>
+                        <InteractionWrapperStyled>
+                            <FormStyled onSubmit={handleSubmit(_handleAuthenticationRedirect)}>
+                                <h3>Sign In To View Favorite Markets</h3>
+                                <InputStyled type='text' name='email' ref={register} placeholder='Email'></InputStyled><br/>
+                                <InputStyled type='password' name='password' ref={register} placeholder='Password'></InputStyled><br/>
+                                <InputButtonStyled type='submit'></InputButtonStyled>
+                            </FormStyled>
+                        </InteractionWrapperStyled>
+                    </InteractionBackground>
+                <ButtonWrapperStyle>
+                    <StyledHeading>-OR-</StyledHeading>
+                    <ButtonStyled onClick={() => history.push('/register')}>Sign Up</ButtonStyled>
                     <ButtonStyled onClick={() => history.push('/main_map')}>Continue as Guest</ButtonStyled>
-                    <ButtonStyled to="/register">Sign Up</ButtonStyled>
-                </InteractionWrapperStyled>
+                </ButtonWrapperStyle>
             </HomeWrapperStyled>
         </Background>
     );
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -2,29 +2,28 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
+const Banner = require('../Home/fmm_banner.png')
 const backgroundImage = require('../../background-home.png')
 
 const RegisterWrapperStyled = styled.div`
     backdrop-filter: blur(3px)  brightness(95%);
     height: 100vh;
-    padding-top: 25vh;
     width: 100%;
     text-align: center;
 `
 const HeaderWrapperStyled = styled.div`
-    height: 10vh;
+    height: 100px;
     width: 100%;
     background: rgb(203,104,93);
-    background: linear-gradient(180deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
-    position: fixed;
-    top: 0px;
+    background: linear-gradient(0deg, rgba(56,163,165,1) 0%, rgba(61,178,180,1) 100%);
+    margin-bottom: 60px;
 `
 
 const InteractionWrapperStyled = styled.div`
     height: 35vh;
     width: 25vw;
     margin: 0 auto;
-    padding: 15px 0;
+    padding: 15px;
     background: #667;
     font-family: arial;
     font-size: 20px;
@@ -36,8 +35,10 @@ const InteractionWrapperStyled = styled.div`
     border-radius: 20px;
 `
 const FormStyled = styled.form`
-    padding: 10px 0;
+    padding: 20px 0 10px 0;
     margin:  auto;
+    
+
 `
 const InputStyled = styled.input`
     font-size: 1.2em;
@@ -45,14 +46,18 @@ const InputStyled = styled.input`
     padding: 0 10px;
     border: 3px solid #38A3A5;
     border-radius: 20px;
+    background: rgb(203,104,93);
+    background: linear-gradient(180deg, rgba(203,104,93,1) 0%, rgba(239,130,117,1) 100%);
+    border-radius: 60px;
 `
 const LabelStyled = styled.label`
 
 `
 
 const InputButtonStyled = styled.input`
+    display:block;
     font-size: 1.2em;
-    margin-top: 10px;
+    margin: 10px auto;
     padding: 3px 10px;
     border: 3px solid rgb(203,104,93);
     border-radius: 20px;
@@ -67,6 +72,12 @@ const Background = styled.div`
     background-repeat: no-repeat;
 `
 
+const BannerStyled = styled.img`
+    width: 30vw;
+    margin-top: 11px;
+`
+
+
 const Register = (({ currentUser, handleUserRegister }) => {
     const { register, handleSubmit } = useForm();
     const history = useHistory();
@@ -79,7 +90,7 @@ const Register = (({ currentUser, handleUserRegister }) => {
     return(
         <Background>
             <RegisterWrapperStyled>
-                <HeaderWrapperStyled></HeaderWrapperStyled>
+                <HeaderWrapperStyled><BannerStyled src={Banner}/></HeaderWrapperStyled>
                 <InteractionWrapperStyled>
                     <h3>Register</h3>
                     <FormStyled onSubmit={handleSubmit(_handleUserRegisterRedirect)}>
