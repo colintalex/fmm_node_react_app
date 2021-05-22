604f29ae292a1e2cbc07d62b686c77a7d665c96f
Colin Alexander <colintalex@gmail.com>
2021-05-22 00:19:20 -0600

    moves neqw build to api

---
+++ api/build/precache-manifest.04472a4b79e1b2e505a5cb11e90f0558.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29d551fbebe4e15d6f91363e219b98e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd8113accaff5d5002b7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5ebedae34a2a530b9e5",
+    "url": "/static/js/2.50d84496.chunk.js"
+  },
+  {
+    "revision": "c12c77ee1e85299c4f4b492072961aca",
+    "url": "/static/js/2.50d84496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8113accaff5d5002b7",
+    "url": "/static/js/main.ff2e587b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "03cb5e4172574620c356efec16e8508f",
+    "url": "/static/media/background-home.03cb5e41.png"
+  },
+  {
+    "revision": "167d65a5ae00cfba429bfdc33177c815",
+    "url": "/static/media/fmm_banner.167d65a5.png"
+  }
+]);
